0ac7a751919a1fd4e7c45527043c820e07adaf48
Daniel456-png <danielsenac276@gmail.com>
2026-05-22 01:47:38 +0000

    Atividade

---
+++ atividade.js
@@ -0,0 +1,33 @@
+ function mensagem() {
+            console.log("Olá, isso é uma mensagem importante");
+ }
+        mensagem();
+
+         function mostrarNome(nome) {
+            console.log("O nome informado foi: " + nome);
+        }
+
+        mostrarNome("Igor");
+
+let nome = "Daneil"
+console.log(nome)
+
+let idade= 16
+console.log("16");
+if (idade >=18){
+   console.log("Maior de idade")
+}else{
+   console.log("Menor de idade")
+}
+
+let nota = 10
+let nota2 = 5
+
+
+let media = ((nota + nota2) / 2)
+
+console.log(media)
+
+
+
+    
